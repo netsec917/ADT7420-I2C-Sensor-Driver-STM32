b848711127300473ea5907995d42aa4483bbfadd
netsec917 <18678888@students.lincoln.ac.uk>
2020-09-05 18:12:46 +0100

    updated ring buffer

--- Core/Inc/ring_buffer.h
+++ Core/Inc/ring_buffer.h
@@ -21,50 +21,50 @@ typedef struct {
 	volatile uint8_t buffer[BUF_SIZE];
 } ring_buffer;
 
-inline uint8_t ring_buffer_mask(ring_buffer* buf, uint8_t val);
-inline void ring_buffer_init(ring_buffer* buf);
-inline void ring_buffer_enqueue(ring_buffer* buf, uint8_t data);
-inline uint8_t ring_buffer_dequeue(ring_buffer* buf);
-inline uint8_t ring_buffer_size(ring_buffer* buf);
-inline bool ring_buffer_empty(ring_buffer* buf);
-inline bool ring_buffer_full(ring_buffer* buf);
+static inline uint8_t ring_buffer_mask(ring_buffer* buf, uint8_t val);
+static inline void ring_buffer_init(ring_buffer* buf);
+static inline void ring_buffer_enqueue(ring_buffer* buf, uint8_t data);
+static inline uint8_t ring_buffer_dequeue(ring_buffer* buf);
+static inline uint8_t ring_buffer_size(ring_buffer* buf);
+static inline bool ring_buffer_empty(ring_buffer* buf);
+static inline bool ring_buffer_full(ring_buffer* buf);
 
 
-inline void ring_buffer_init(ring_buffer* buf)
+static inline void ring_buffer_init(ring_buffer* buf)
 {
 	buf->read = 0;
 	buf->write = 0;
 	memset(buf, 0, BUF_SIZE);
 }
 
-inline uint8_t ring_buffer_mask(ring_buffer* buf, uint8_t val)
+static inline uint8_t ring_buffer_mask(ring_buffer* buf, uint8_t val)
 {
 	return val & (BUF_SIZE - 1);
 }
 
-inline void ring_buffer_enqueue(ring_buffer* buf, uint8_t data)
+static inline void ring_buffer_enqueue(ring_buffer* buf, uint8_t data)
 {
 	assert(!ring_buffer_full(buf));
 	buf->buffer[ring_buffer_mask(buf, buf->write++)] = data;
 }
 
-inline uint8_t ring_buffer_dequeue(ring_buffer* buf)
+static inline uint8_t ring_buffer_dequeue(ring_buffer* buf)
 {
 	assert(!ring_buffer_empty(buf));
 	return buf->buffer[ring_buffer_mask(buf, buf->read++)];
 }
 
-inline uint8_t ring_buffer_size(ring_buffer* buf)
+static inline uint8_t ring_buffer_size(ring_buffer* buf)
 {
 	return buf->write - buf->read;
 }
 
-inline bool ring_buffer_empty(ring_buffer* buf)
+static inline bool ring_buffer_empty(ring_buffer* buf)
 {
 	return buf->read == buf->write;
 }
 
-inline bool ring_buffer_full(ring_buffer* buf)
+static inline bool ring_buffer_full(ring_buffer* buf)
 {
 	return ring_buffer_size(buf) == BUF_SIZE;
 }
